27b1f5c081b9aab7f9719901560786c5f0faa33a
Taylor-Cameron <taylorcameron95@hotmail.ca>
2018-09-29 16:07:11 +0000

    adds looping exercises

---
+++ chorus-for.js
@@ -0,0 +1,7 @@
+var chorus = "Let's dance!";
+// logs chorus 10 times
+for(var repeat = 0; repeat < 10; repeat++){
+  console.log(chorus);
+}
+
+console.log("Until the sun comes up!");
---
+++ packing-list.js
@@ -0,0 +1,10 @@
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log('Kitchen stuff to pack');
+/*logs the array packingList from index 0 - until (i < packingList.length)
+is no longer true*/
+for(var i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+packingList[3];
